fecc5b0d72bf2c31aa6a9423b8e459516b16e6c6
kpjackson2013 <57926455+kpjackson2013@users.noreply.github.com>
2020-08-26 19:56:48 -0400

    userroutes

--- index.js
+++ index.js
@@ -20,5 +20,106 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
 });
+app.post("/user",async(request,response)=>{
+  try {
+    console.log("postuser") 
+    if (!request.body.username||!request.body.firstName||!request.body.lastName||!request.body.role) {
+      response.status(400).send({ message: 'enter all required information' });
+    }
+
+    const con = await pool.getConnection();
+    const queryResponse = await con.execute(
+      'INSERT INTO homeworkapp.user (username, firstName, lastName, role) VALUES (?, ?, ?, ?)',
+      [
+        request.body.username,
+        request.body.firstName , request.body.lastName , request.body.role       ]
+    );
+    con.release();
+
+    console.log(queryResponse);
+
+    response.status(200).send({ messge: queryResponse });
+
+  } catch (error) {
+    console.log (error)
+    response.status(500).send({message:error})
+  }
+})
+
+
+app.get('/user', authorizeUser, async (request, response) => {
+  try {
+    console.log('GET ONE USER');
+
+    /*const email = request.decodedToken.email;
+    if (!email) {
+      response.status(400).send({ message: 'access denied' });
+    }*/
+
+    const con = await pool.getConnection();
+    const recordset = await con.execute(
+      'SELECT * FROM homeworkapp.user WHERE username=?',
+      [request.body.username]
+    );
+    con.release();
+
+    console.log(recordset[0]);
+
+    response.status(200).send({ message: recordset[0] });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ error: error.message, message: error });
+  }
+});
+
+
+function authorizeUser(request, response, next) {
+  console.log('AuthroizeUser');
+
+  /*if (request.query.token) request.body.token = request.query.token;
+  const tokenFromRequestBody = request.body.token;*/
+
+  try {
+    /*jwt.verify(tokenFromRequestBody, pem, function (error, decodedToken) {
+      if (error) {
+        console.log(error);
+        return response.status(403).send(error);
+      }
+
+      request.decodedToken = decodedToken;
+    });*/
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send(error);
+  }
+}
+
+app.post("/student", authorizeUser, async(request,response)=>{
+  try {
+    console.log("poststudentinfo") 
+    if (!request.body.username||!request.body.firstName||!request.body.lastName||!request.body.role) {
+      response.status(400).send({ message: 'enter all required information' });
+    }
+
+    const con = await pool.getConnection();
+    const queryResponse = await con.execute(
+      'INSERT INTO homeworkapp.user (username, firstName, lastName, role) VALUES (?, ?, ?, ?)',
+      [
+        request.body.username,
+        request.body.firstName , request.body.lastName , request.body.role       ]
+    );
+    con.release();
+
+    console.log(queryResponse);
+
+    response.status(200).send({ messge: queryResponse });
+
+  } catch (error) {
+    console.log (error)
+    response.status(500).send({message:error})
+  }
+})
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
