4fffd8ff868bbd78bdd7bea24efa74a640184a47
Nitika Chaudhary <nitika.hopkins@gmail.com>
2020-08-26 20:37:40 -0400

    added student and teacher route for post

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2/promise');
-const { response } = require('express');
-const jwt = require('jsonwebtoken');
-const jwkToPem = require('jwk-to-pem');
-const aws = require('aws-sdk');
-const asyncMap = require('./helpers');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+const { response } = require("express");
+const jwt = require("jsonwebtoken");
+const jwkToPem = require("jwk-to-pem");
+const aws = require("aws-sdk");
+const asyncMap = require("./helpers");
 const PORT = 4000;
 
 const app = express();
@@ -18,38 +18,44 @@ app.use(cors());
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
 });
-app.post("/user",async(request,response)=>{
+app.post("/user", async (request, response) => {
   try {
-    console.log("postuser") 
-    if (!request.body.username||!request.body.firstName||!request.body.lastName||!request.body.role) {
-      response.status(400).send({ message: 'enter all required information' });
+    console.log("postuser");
+    if (
+      !request.body.username ||
+      !request.body.firstName ||
+      !request.body.lastName ||
+      !request.body.role
+    ) {
+      response.status(400).send({ message: "enter all required information" });
     }
 
     const con = await pool.getConnection();
     const queryResponse = await con.execute(
-      'INSERT INTO homeworkapp.user (username, firstName, lastName, role) VALUES (?, ?, ?, ?)',
+      "INSERT INTO homeworkapp.user (username, firstName, lastName, role) VALUES (?, ?, ?, ?)",
       [
         request.body.username,
-        request.body.firstName , request.body.lastName , request.body.role       ]
+        request.body.firstName,
+        request.body.lastName,
+        request.body.role,
+      ]
     );
     con.release();
 
     console.log(queryResponse);
 
     response.status(200).send({ messge: queryResponse });
-
   } catch (error) {
-    console.log (error)
-    response.status(500).send({message:error})
+    console.log(error);
+    response.status(500).send({ message: error });
   }
-})
+});
 
-
-app.get('/user', authorizeUser, async (request, response) => {
+app.get("/user", authorizeUser, async (request, response) => {
   try {
-    console.log('GET ONE USER');
+    console.log("GET ONE USER");
 
     /*const email = request.decodedToken.email;
     if (!email) {
@@ -58,8 +64,8 @@ app.get('/user', authorizeUser, async (request, response) => {
 
     const con = await pool.getConnection();
     const recordset = await con.execute(
-      'SELECT * FROM homeworkapp.user WHERE username=?',
-      [request.body.username]
+      "SELECT * FROM homeworkapp.user WHERE username=?",
+      [request.query.username]
     );
     con.release();
 
@@ -72,9 +78,8 @@ app.get('/user', authorizeUser, async (request, response) => {
   }
 });
 
-
 function authorizeUser(request, response, next) {
-  console.log('AuthroizeUser');
+  console.log("AuthroizeUser");
 
   /*if (request.query.token) request.body.token = request.query.token;
   const tokenFromRequestBody = request.body.token;*/
@@ -96,30 +101,60 @@ function authorizeUser(request, response, next) {
   }
 }
 
-app.post("/student", authorizeUser, async(request,response)=>{
+app.post("/student", authorizeUser, async (request, response) => {
   try {
-    console.log("poststudentinfo") 
-    if (!request.body.username||!request.body.firstName||!request.body.lastName||!request.body.role) {
-      response.status(400).send({ message: 'enter all required information' });
-    }
+    console.log("poststudentinfo");
 
     const con = await pool.getConnection();
     const queryResponse = await con.execute(
-      'INSERT INTO homeworkapp.user (username, firstName, lastName, role) VALUES (?, ?, ?, ?)',
+      "INSERT INTO homeworkapp.student ( username, skype, bio, birthday, profilepic) VALUES ( ?, ?, ?, ?, ?)",
       [
         request.body.username,
-        request.body.firstName , request.body.lastName , request.body.role       ]
+        request.body.skype ? request.body.skype : null,
+        request.body.bio ? request.body.bio : null,
+        request.body.birthday ? request.body.birthday : null,
+        request.body.profilepic ? request.body.profilepic : null,
+      ]
     );
     con.release();
 
     console.log(queryResponse);
 
     response.status(200).send({ messge: queryResponse });
-
   } catch (error) {
-    console.log (error)
-    response.status(500).send({message:error})
+    console.log(error);
+    response.status(500).send({ message: error });
   }
-})
+});
+
+app.post("/teacher", authorizeUser, async (request, response) => {
+  try {
+    console.log("poststudentinfo");
+
+    const con = await pool.getConnection();
+    const queryResponse = await con.execute(
+      "INSERT INTO homeworkapp.teacher ( username, skype, bio, birthday, profilepic, subject, experience, method, price) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        request.body.username,
+        request.body.skype ? request.body.skype : null,
+        request.body.bio ? request.body.bio : null,
+        request.body.birthday ? request.body.birthday : null,
+        request.body.profilepic ? request.body.profilepic : null,
+        request.body.subject ? request.body.subject : null,
+        request.body.experience ? request.body.experience : null,
+        request.body.method ? request.body.method : null,
+        request.body.price ? request.body.price : null,
+      ]
+    );
+    con.release();
+
+    console.log(queryResponse);
+
+    response.status(200).send({ messge: queryResponse });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error });
+  }
+});
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
